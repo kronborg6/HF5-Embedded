b9c104b756d588f6a27f17b9b121b3d61e59abbc
Kronborg <mkronborg7@gmail.com>
2023-01-11 10:59:23 +0100

    add data model

--- goEmbeddedApi/api/models/data.go
+++ goEmbeddedApi/api/models/data.go
@@ -4,5 +4,8 @@ import "time"
 
 type Data struct {
 	Id        int       `json:"id" gorm:"primaryKey"`
+	Local     string    `json:"local"`
 	TimeStamp time.Time `json:"time_stamp"`
+	Temp      int       `json:"temp"`
+	Humidity  int       `json:"humidity"`
 }
