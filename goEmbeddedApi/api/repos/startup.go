c37b933342b463b5c7137f051da8a5dba2572afb
Kronborg <mkronborg7@gmail.com>
2023-01-12 14:21:53 +0100

    start on startup repo

--- goEmbeddedApi/api/repos/startup.go
+++ goEmbeddedApi/api/repos/startup.go
@@ -1,1 +1,20 @@
 package repos
+
+import (
+	"github.com/kronborg6/HF5-Embedded/goEmbeddedApi/api/models"
+	"gorm.io/gorm"
+)
+
+type StartupRepo struct {
+	db *gorm.DB
+}
+
+func (repo *StartupRepo) GetAllStartup() ([]models.Startup, error) {
+	var startup []models.Startup
+
+	if err := repo.db.Debug().Find(&startup).Error; err != nil {
+		return startup, err
+	}
+
+	return startup, nil
+}
