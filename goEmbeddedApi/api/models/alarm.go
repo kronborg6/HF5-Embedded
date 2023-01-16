0cbd4283601ab813416bb545d857b77f812c6ae5
Kronborg <mkronborg7@gmail.com>
2023-01-16 11:01:04 +0100

    start on seeding

--- goEmbeddedApi/api/models/alarm.go
+++ goEmbeddedApi/api/models/alarm.go
@@ -4,8 +4,10 @@ import "time"
 
 type Alarm struct {
 	Id        int       `json:"id" gorm:"primaryKey"`
-	Local     string    `json:"local"`
+	LocalName string    `json:"local_id"`
+	Local     Startup   `gorm:"foreignKey:LocalName"`
 	TimeStamp time.Time `json:"time_stamp"`
 	TypeId    int       `json:"type_id"`
 	Type      Types     `gorm:"foreignKey:TypeId"`
+	Value     int       `json:"value"`
 }
--- goEmbeddedApi/api/models/tableStartup.go
+++ goEmbeddedApi/api/models/tableStartup.go
@@ -1,18 +1,68 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 func Setup(db *gorm.DB) {
 	db.Migrator().DropTable(
 		&Types{},
-		&Alarm{},
 		&Startup{},
 		&Data{},
+		&Alarm{},
 	)
 	db.AutoMigrate(
 		&Types{},
-		&Alarm{},
 		&Startup{},
 		&Data{},
+		&Alarm{},
 	)
+
+	startup := []Startup{
+		{
+			Local:         "Drivehus 1",
+			StartTime:     time.Now(),
+			EndTime:       time.Now(),
+			MinTemp:       5,
+			MaxTemp:       15,
+			MinHumidity:   10,
+			MaxHumidity:   20,
+			MaxNoiseLevel: 75,
+		},
+		{
+			Local:         "Drivehus 2",
+			StartTime:     time.Now(),
+			EndTime:       time.Now(),
+			MinTemp:       5,
+			MaxTemp:       15,
+			MinHumidity:   10,
+			MaxHumidity:   20,
+			MaxNoiseLevel: 75,
+		},
+	}
+	types := []Types{
+		{
+			Type: "Sound",
+		},
+		{
+			Type: "Temp",
+		},
+		{
+			Type: "Humidity",
+		},
+	}
+	// alarm := []Alarm{
+	// 	{
+	// 		LocalName: "DriveHus 2",
+	// 		TimeStamp: time.Now(),
+	// 		TypeId:    1,
+	// 		Value:     100,
+	// 	},
+	// }
+
+	db.Create(&startup)
+	// db.Create(&alarm)
+	db.Create(&types)
 }
