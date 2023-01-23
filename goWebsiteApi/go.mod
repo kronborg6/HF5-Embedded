2d3b6bd4279e3ec23af99b79a65b5096e9727a29
Kronborg <mkronborg7@gmail.com>
2023-01-23 18:53:22 +0100

    jwt test

--- goWebsiteApi/go.mod
+++ goWebsiteApi/go.mod
@@ -6,17 +6,19 @@ require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/gofiber/fiber/v2 v2.41.0 // indirect
+	github.com/gofiber/jwt/v3 v3.3.5 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/klauspost/compress v1.15.15 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/rivo/uniseg v0.4.3 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.43.0 // indirect
+	github.com/valyala/fasthttp v1.44.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	golang.org/x/sys v0.4.0 // indirect
 	gorm.io/driver/mysql v1.4.5 // indirect
 	gorm.io/gorm v1.24.3 // indirect
 )
--- goWebsiteApi/main.go
+++ goWebsiteApi/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	jwtware "github.com/gofiber/jwt/v3"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/kronborg6/HF5-Embedded/goWebsiteApi/api/controllers"
 	"github.com/kronborg6/HF5-Embedded/goWebsiteApi/api/db"
 	"github.com/kronborg6/HF5-Embedded/goWebsiteApi/api/middleware"
 	"github.com/kronborg6/HF5-Embedded/goWebsiteApi/api/models"
+)
+
+var (
+	// Obviously, this is just a test example. Do not do this in production.
+	// In production, you would have the private key and public key pair generated
+	// in advance. NEVER add a private key to any GitHub repo.
+	privateKey = "dfg"
 )
 
 func main() {
@@ -22,9 +33,36 @@ func main() {
 	app.Use(logger.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
+
+		/* 		return c.JSON(fiber.Map{
 			"message": "this is a endpoint test",
 			"test":    "Test",
+		}) */
+		claims := jwt.MapClaims{
+			"name":  "John Doe",
+			"admin": true,
+			"exp":   time.Now().Add(time.Hour * 72).Unix(),
+		}
+
+		// Create token
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+		// Generate encoded token and send it as response.
+		t, err := token.SignedString([]byte("secret"))
+		if err != nil {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+
+		return c.JSON(fiber.Map{"token": t})
+	})
+
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: []byte("secret"),
+	}))
+
+	app.Get("/no", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "fuck no",
 		})
 	})
 
@@ -35,6 +73,6 @@ func main() {
 
 	controllers.RegisterDataController(db, api)
 
-	// log.Fatal(app.Listen(":8000"))
+	log.Fatal(app.Listen(":8000"))
 
 }
