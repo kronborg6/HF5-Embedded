9bc51c4616361299bfcc239bfc6d83d9d4e174b7
aksschnell <54832919+aksschnell@users.noreply.github.com>
2023-01-24 14:23:47 +0100

    tirsdag

--- Website/myproject/hf5/urls.py
+++ Website/myproject/hf5/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("warnings", views.warnings, name="warnings")
+    path("warnings", views.warnings, name="warnings"),
+    path("login", views.login, name="login"),
+    path("logout", views.logout, name="logout"),
+    path("startup", views.startup, name="startup"),
 ]
